47a94cb7bb4b4b894bc488b3adf4b50f9dd83a28
shuhei23 <shuhei.toda@gmail.com>
2022-07-18 23:51:51 +0900

    必ずコンストラクタで初期化する

--- src/main/java/com/example/chapter3/Money.java
+++ src/main/java/com/example/chapter3/Money.java
@@ -5,4 +5,9 @@ import java.util.Currency;
 public class Money {
     int amount;
     Currency currency;
+
+    Money(int amount, Currency currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
 }
