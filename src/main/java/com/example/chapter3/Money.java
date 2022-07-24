13d24855cb311b701c6ff8bcd1b1ed77d9602409
shuhei23 <shuhei.toda@gmail.com>
2022-07-25 00:02:20 +0900

    final で不変にする

--- src/main/java/com/example/chapter3/Money.java
+++ src/main/java/com/example/chapter3/Money.java
@@ -3,8 +3,8 @@ package com.example.chapter3;
 import java.util.Currency;
 
 public class Money {
-    int amount;
-    Currency currency;
+    final int amount;
+    final Currency currency;
 
     Money(int amount, Currency currency) {
         if (amount < 0) {
