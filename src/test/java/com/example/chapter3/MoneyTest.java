c5a546e4461c6c65badc9c89360e0dd10e36323a
shuhei23 <shuhei.toda@gmail.com>
2022-07-24 23:57:34 +0900

    インスタンス変数をどんどん上書きしている

--- src/test/java/com/example/chapter3/MoneyTest.java
+++ src/test/java/com/example/chapter3/MoneyTest.java
@@ -2,6 +2,7 @@ package com.example.chapter3;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Calendar;
 import java.util.Currency;
 import java.util.Locale;
 
@@ -22,4 +23,33 @@ public class MoneyTest {
                 () -> new Money(10, null));
         assertEquals("通貨単位を指定してください。", e.getMessage());
     }
+
+    int originalPrice = 100;
+    int additionalServiceFee = 20;
+    boolean specialServiceAdded = true;
+    boolean seasonOffApplied = true;
+
+    private int seasonPrice() {
+        Calendar calendar = Calendar.getInstance();
+        int month = calendar.get(Calendar.MONTH) + 1;
+        return month * 10;
+    }
+
+    @Test
+    void インスタンス変数をどんどん上書きしている() {
+
+        Money money = new Money(0, Currency.getInstance(Locale.JAPAN));
+        money.amount = originalPrice;
+        // 中略
+        if (specialServiceAdded) {
+            money.add(additionalServiceFee);
+            // 中略
+            if (seasonOffApplied) {
+                money.amount = seasonPrice();
+            }
+        }
+
+        System.out.println(String.format("料金は%d円です。",money.amount));
+    }
+
 }
